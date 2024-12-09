fb970adda8540fe628e3c44e319be1739c539a8b
GatlenCulp <GatlenCulp@gmail.com>
2024-12-09 14:23:49 -0500

    Small changes to loss summary and aggregate statistics

--- src/schema/AggregateStatistics.py
+++ src/schema/AggregateStatistics.py
@@ -1,6 +1,5 @@
 """Aggregate statistics across all the stitches."""
 
-
 from pydantic import BaseModel
 from pydantic import Field
 
@@ -8,7 +7,12 @@ from src.schema.training_schemas import StitchSummary
 
 
 class AggregateStatistics(BaseModel):
-    """Aggregate statistics across all the stitches. Can be rednered into table."""
+    """Aggregate statistics across all the stitches. Can be rendered into table.
+
+    NOTE: Probably want to use some sort of order between the final results
+    """
 
     # Input
     summaries: list[StitchSummary] = Field(description="Literally every training run as input.")
+
+
