6746ba94331a7e91f97cc76ae97ed8da6d27fde0
Adriano Hernandez <adrianoh@mit.edu>
2024-12-10 13:23:45 -0500

    chunking fix part 1

--- owler_fork/owlergpt/commands/__init__.py
+++ owler_fork/owlergpt/commands/__init__.py
@@ -9,3 +9,4 @@ from .create_openai_embeddings import ingest_openai
 from .create_regular_embeddings import ingest_hf
 from .shrink_random_subset_of_dataset import shrink
 from .split_validation import split_validation
+from .chunk_dataset import chunk_dataset
